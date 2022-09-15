a169dfa00e6142fd41a0740a6e8640f1290790b9
Sumaiya-sumu967 <96183556+Sumaiya-sumu967@users.noreply.github.com>
2022-09-16 01:58:27 +0600

    first commit

---
+++ src/package1/VolumeMeasure.java
@@ -0,0 +1,23 @@
+package package1;
+
+public class VolumeMeasure {
+	   public double v;
+	   //public double V;
+	   public double pi=3.1416;
+	   
+	   public double measureVolume(double r, double h) {
+	        
+		 return v = pi*(r*r)*h;  //Volume of Cylinder v=πr2h
+		 
+		 
+	    }	
+	   public double measureVolume(double r) {
+	        
+			 return v = (4/3)*pi*(r*r*r);  //Volume of sphere V=4/3πr3
+	   }
+       public double measureVolume(int a) {
+			        
+		     return v = a*a*a;  //VVolume of Cube V=a3
+       } 
+};
+	   
---
+++ src/package1/main.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class main {
+	public main(String[] args){
+	     VolumeMeasure obj = new VolumeMeasure();
+	     System.out.println("Volume of Cylinder is : "+ obj.measureVolume(10,100));
+	     System.out.println("Volume of sphere is : "+ obj.measureVolume(100));
+	     System.out.println("Volume of Cube is : "+ obj.measureVolume(10));
+	     
+	    
+	}
+}
